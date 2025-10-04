57ca7d7fa63f2e153e879d5137a399baf655e0f8
NANDINISHARMA30 <nandiniisharma2005@gmail.com>
2025-10-04 23:15:13 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Hello Tailwind CSS with React & Vite!
+      </h1>
+    </div>
+  );
+}
+
+
+export default App
